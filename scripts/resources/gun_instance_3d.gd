421d03387c654fc6fa8a9668e8421154f387aa3e
fat-ranga <kael.robertson1257@gmail.com>
2023-05-05 17:01:15 +1200

    Connect AnimationPlayer signal automatically

--- scripts/resources/gun_instance_3d.gd
+++ scripts/resources/gun_instance_3d.gd
@@ -37,6 +37,7 @@ var is_firing: bool = false
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
+	animation_player.animation_finished.connect(_on_animation_player_animation_finished)
 	pass # Replace with function body.
 
 func unequip() -> void:
